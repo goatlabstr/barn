d111374a570e2554abaaf6751959b0b8dd9f6283
NvztCRK <nevzatcirak17@hotmail.com>
2022-06-30 00:54:01 +0300

    Configs are loaded dynamically.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {BrowserRouter as Router} from "react-router-dom";
 import Main from "./Main";
 import CustomThemeProvider from "./context/Theme/CustomThemeProvider";
@@ -12,46 +12,46 @@ import {Collapse} from "@mui/material";
 import Common from "./services/axios/common";
 
 function App() {
+    const [configReady, setConfigReady] = useState<boolean>(false);
     const {activate, passivate} = useGlobalPreloader();
 
     useEffect(() => {
-        if (sessionStorage.getItem(window.location.hostname.split(".goatlabs.zone")[0] + "-barn-configuration") === undefined) {
+        if (sessionStorage.getItem(window.location.hostname.split(".goatlabs.zone")[0] + "-barn-configuration") === null) {
             activate();
             Common.getConfig().then(res => {
                 sessionStorage.setItem(window.location.hostname.split(".goatlabs.zone")[0] + "-barn-configuration",
                     JSON.stringify(res.data));
+                setConfigReady(true);
                 passivate();
             });
-        }
+        } else
+            setConfigReady(true);
     }, []);
 
-    return (<>
-        {
-            sessionStorage.getItem(window.location.hostname.split(".goatlabs.zone")[0] + "-barn-configuration") ?
-                <Router basename={"/"}>
-                    <AppStateProvider>
-                        <CustomThemeProvider>
-                            <GlobalPreloaderProvider>
-                                <SnackbarProvider
-                                    maxSnack={5}
-                                    anchorOrigin={{
-                                        vertical: "bottom",
-                                        horizontal: "right",
-                                    }}
-                                    dense
-                                    preventDuplicate
-                                    TransitionComponent={Collapse}
-                                >
-                                    <DialogProvider>
-                                        <Main/>
-                                    </DialogProvider>
-                                </SnackbarProvider>
-                            </GlobalPreloaderProvider>
-                        </CustomThemeProvider>
-                    </AppStateProvider>
-                </Router> : <div/>
-        }
-    </>);
+    return (
+        <Router basename={"/"}>
+            <AppStateProvider>
+                <CustomThemeProvider>
+                    <GlobalPreloaderProvider>
+                        <SnackbarProvider
+                            maxSnack={5}
+                            anchorOrigin={{
+                                vertical: "bottom",
+                                horizontal: "right",
+                            }}
+                            dense
+                            preventDuplicate
+                            TransitionComponent={Collapse}
+                        >
+                            {configReady ? <DialogProvider>
+                                <Main/>
+                            </DialogProvider> : <div/>}
+                        </SnackbarProvider>
+                    </GlobalPreloaderProvider>
+                </CustomThemeProvider>
+            </AppStateProvider>
+        </Router>
+    );
 }
 
 export default App;
