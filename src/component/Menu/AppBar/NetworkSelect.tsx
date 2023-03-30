e2aaf683b04f635e2bb94c29ff668edc52c64e6f
NvztCRK <nevzatcirak17@hotmail.com>
2023-03-30 03:06:07 +0300

    network switch drawer is added.

--- src/component/Menu/AppBar/NetworkSelect.tsx
+++ src/component/Menu/AppBar/NetworkSelect.tsx
@@ -2,32 +2,22 @@ import * as React from 'react';
 import {useEffect, useState} from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
 import {useTranslation} from "react-i18next";
-import {Avatar, InputAdornment, Stack, SwipeableDrawer, Typography} from "@mui/material";
+import {Avatar, Chip, Stack, SwipeableDrawer} from "@mui/material";
 import {useAppState} from "../../../hooks/useAppState";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import Common from "../../../services/axios/common";
-import {networkName} from "../../../constants/networkConfig";
-import Autocomplete from "@mui/material/Autocomplete";
-import TextField from "@mui/material/TextField";
 import {darken, lighten, styled} from '@mui/system';
 import SearchIcon from '@mui/icons-material/Search';
 
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
 
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
 
 const GroupHeader = styled('div')(({theme}) => ({
     position: 'sticky',
@@ -48,7 +38,6 @@ const GroupItems = styled('ul')({
 export default function NetworkSelect({type = "medium"}: { type?: "medium" | "small" | "large" | undefined }) {
     const {t} = useTranslation();
     const [open, setOpen] = React.useState(false);
-    const [network, setNetwork] = React.useState<object | null>(null);
     const {
         appState: {
             chainInfo,
@@ -63,10 +52,6 @@ export default function NetworkSelect({type = "medium"}: { type?: "medium" | "sm
         Common.getAllChainsInfo().then(resp => {
             const chainsData = resp?.data?.chains;
             setSupportedChainConfigs(chainsData);
-            //@ts-ignore
-            const filteredChainData = chainsData?.filter(o => o?.name === networkName);
-            if (filteredChainData !== undefined && filteredChainData.length > 0)
-                setNetwork(filteredChainData[0]);
         });
     }, [])
 
@@ -89,9 +74,6 @@ export default function NetworkSelect({type = "medium"}: { type?: "medium" | "sm
     const handleSearchText = (search: string) => {
         setSearchText(search);
     }
-    const handleChange = (network: object | null) => {
-        setNetwork(network);
-    };
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -109,77 +91,31 @@ export default function NetworkSelect({type = "medium"}: { type?: "medium" | "sm
         setOpen(false);
     };
 
-    const handleSwitch = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-        if (reason !== 'backdropClick') {
+    const onNetworkSwitch = (networkName) => {
+        if (networkName) {
             setOpen(false);
             //@ts-ignore
-            window.location.href = window.location.origin + "/" + network?.name;
+            let lastViewedNetworks = JSON.parse(window.localStorage.getItem("goat-last-viewed-networks"));
+            if (!lastViewedNetworks)
+                lastViewedNetworks = [];
+            if (!lastViewedNetworks.includes(networkName))
+                lastViewedNetworks.push(networkName);
+            //@ts-ignore
+            window.localStorage.setItem("goat-last-viewed-networks", JSON.stringify(lastViewedNetworks));
+            //@ts-ignore
+            window.location.href = window.location.origin + "/" + networkName;
         }
     };
 
-    const onNetworkSwitch = (network) => {
-        setOpen(false);
+    const getLastViewedNetworks = () => {
         //@ts-ignore
-        window.location.href = window.location.origin + "/" + network?.name;
-    };
-
-    const NetworkDialog = () => {
-        return (
-            <Dialog disableEscapeKeyDown open={open} onClose={handleClose} fullWidth>
-                <DialogTitle>Switch Network</DialogTitle>
-                <DialogContent>
-                    <Stack direction={"row"} alignItems={"center"} sx={{p: 1}}>
-                        {network && <Avatar
-                            sx={{width: 35, height: 35, ml: 1, mr: 1.5}}
-                            //@ts-ignore
-                            src={network?.image}
-                        />}
-                        <Autocomplete
-                            id="switch-network-auto-complete"
-                            //@ts-ignore
-                            options={chainConfigs.sort((a, b) => b?.status.localeCompare(a?.status))}
-                            //@ts-ignore
-                            groupBy={(option) => option?.network_type}
-                            renderGroup={(params) => (
-                                <li key={params.key}>
-                                    <GroupHeader>{params.group}</GroupHeader>
-                                    <GroupItems>{params.children}</GroupItems>
-                                </li>
-                            )}
-                            //@ts-ignore
-                            getOptionLabel={(option) => option?.name}
-                            renderOption={(props, option) => (
-                                <Box component="li" sx={{'& > img': {mr: 2, flexShrink: 0}}} {...props}>
-                                    <Stack direction={"row"}>
-                                        <Avatar
-                                            sx={{width: 30, height: 30, ml: 1, mr: 1.5, zIndex: 0}}
-                                            //@ts-ignore
-                                            src={option?.image}
-                                        />
-                                    </Stack>
-                                    <Typography variant={"subtitle1"}>
-                                        {//@ts-ignore
-                                            option?.pretty_name}
-                                    </Typography>
-                                </Box>
-                            )}
-                            fullWidth
-                            value={network}
-                            onChange={(event: any, newValue: object | null) => {
-                                handleChange(newValue);
-                            }}
-                            renderInput={(params) => <TextField {...params} label="Network"/>}
-                        />
-                    </Stack>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>{t("cancel")}</Button>
-                    <Button disabled={!network} onClick={handleSwitch}>{t("switch")}</Button>
-                </DialogActions>
-            </Dialog>
-        )
+        const networks = JSON.parse(window.localStorage.getItem("goat-last-viewed-networks"));
+        if (networks) {
+            //@ts-ignore
+            return chainConfigs.filter(c => networks?.includes(c?.name));
+        }
+        return [];
     }
-
     const NetworkDrawer = () => {
         return (
             <SwipeableDrawer
@@ -204,11 +140,23 @@ export default function NetworkSelect({type = "medium"}: { type?: "medium" | "sm
                         onChange={(e) => handleSearchText(e.target.value)}
                     />
                 </Paper>
+                <Stack direction={"row"} spacing={1}
+                       sx={{width: '100%', p: 1, overflowX: "scroll", bgcolor: 'background.paper'}}>
+                    {getLastViewedNetworks().map(c => (
+                        <Chip avatar={<Avatar //@ts-ignore
+                            src={c?.image}
+                        />}
+                            //@ts-ignore
+                              label={c?.pretty_name}
+                            //@ts-ignore
+                              onClick={() => onNetworkSwitch(c?.name)}
+                              variant="outlined"/>))}
+                </Stack>
                 <Box sx={{width: '100%', maxHeight: 300, overflowY: "scroll", bgcolor: 'background.paper'}}>
                     <List>
                         {//@ts-ignore
                             searchResult.sort((a, b) => b?.status.localeCompare(a?.status)).map(config => (
-                                <ListItem disablePadding onClick={() => onNetworkSwitch(config)}>
+                                <ListItem disablePadding onClick={() => onNetworkSwitch(config?.name)}>
                                     <ListItemButton>
                                         <ListItemIcon>
                                             <Avatar
